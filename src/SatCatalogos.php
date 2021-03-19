5b15e5a4037c502b85e4761ab923c48c209a285b
Carlos C Soto <eclipxe13@gmail.com>
2021-03-19 12:37:40 -0600

    Refactor SatCatalogs to have hard definitions (avoid magic)
    This will allow to have method names different than class names.

--- src/SatCatalogos.php
+++ src/SatCatalogos.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpCfdi\SatCatalogos;
 
+use LogicException;
 use PhpCfdi\SatCatalogos\Common\BaseCatalog;
 use PhpCfdi\SatCatalogos\Exceptions\SatCatalogosLogicException;
 
@@ -44,64 +45,95 @@ use PhpCfdi\SatCatalogos\Exceptions\SatCatalogosLogicException;
  */
 class SatCatalogos
 {
-    /** @var array<string, mixed> */
-    protected $cache;
+    /** @var array<string, class-string|BaseCatalog> */
+    private $map = [
+        // CFDI
+        'aduanas' => CFDI\Aduanas::class,
+        'clavesUnidades' => CFDI\ClavesUnidades::class,
+        'codigosPostales' => CFDI\CodigosPostales::class,
+        'formasDePago' => CFDI\FormasDePago::class,
+        'impuestos' => CFDI\Impuestos::class,
+        'metodosDePago' => CFDI\MetodosDePago::class,
+        'monedas' => CFDI\Monedas::class,
+        'numerosPedimentoAduana' => CFDI\NumerosPedimentoAduana::class,
+        'paises' => CFDI\Paises::class,
+        'patentesAduanales' => CFDI\PatentesAduanales::class,
+        'productosServicios' => CFDI\ProductosServicios::class,
+        'regimenesFiscales' => CFDI\RegimenesFiscales::class,
+        'reglasTasaCuota' => CFDI\ReglasTasaCuota::class,
+        'tiposComprobantes' => CFDI\TiposComprobantes::class,
+        'tiposFactores' => CFDI\TiposFactores::class,
+        'tiposRelaciones' => CFDI\TiposRelaciones::class,
+        'usosCfdi' => CFDI\UsosCfdi::class,
+        // Nominas
+        'tiposContratos' => Nomina\TiposContratos::class,
+        'tiposNominas' => Nomina\TiposNominas::class,
+        'tiposJornadas' => Nomina\TiposJornadas::class,
+        'origenesRecursos' => Nomina\OrigenesRecursos::class,
+        'bancos' => Nomina\Bancos::class,
+        'periodicidadesPagos' => Nomina\PeriodicidadesPagos::class,
+        'riesgosPuestos' => Nomina\RiesgosPuestos::class,
+        'tiposDeducciones' => Nomina\TiposDeducciones::class,
+        'tiposHoras' => Nomina\TiposHoras::class,
+        'tiposIncapacidades' => Nomina\TiposIncapacidades::class,
+        'tiposOtrosPagos' => Nomina\TiposOtrosPagos::class,
+        'tiposPercepciones' => Nomina\TiposPercepciones::class,
+        'tiposRegimenes' => Nomina\TiposRegimenes::class,
+    ];
 
     /** @var Repository */
     private $repository;
 
     public function __construct(Repository $repository)
     {
-        $this->cache = [];
         $this->repository = $repository;
     }
 
     /**
      * Magic method to return a catalog using the method name
      *
-     * @param string $name
+     * @param string $methodName
      * @param mixed[] $arguments
      * @return mixed
      * @throws SatCatalogosLogicException if cannot find a matching catalog with the method name
      */
-    public function __call(string $name, array $arguments)
+    public function __call(string $methodName, array $arguments)
     {
-        if (isset($this->cache[$name])) {
-            return $this->cache[$name];
+        if (! isset($this->map[$methodName])) {
+            throw new SatCatalogosLogicException("No se pudo encontrar el catálogo '$methodName'");
         }
 
-        /** @var mixed $created */
-        $created = $this->create($name);
-        if (null !== $created) {
-            $this->cache[$name] = $created;
-            return $created;
+        if (is_object($this->map[$methodName])) {
+            return $this->map[$methodName];
         }
 
-        throw new SatCatalogosLogicException("No se pudo encontrar el catálogo '$name'");
+        try {
+            /** @var mixed $created */
+            $created = $this->create($this->map[$methodName]);
+        } catch (LogicException $exception) {
+            throw new SatCatalogosLogicException("No se pudo encontrar el catálogo '$methodName'", 0, $exception);
+        }
+
+        $this->map[$methodName] = $created;
+        return $created;
     }
 
     /**
-     * @param string $propertyName
-     * @return BaseCatalog|null
+     * @param class-string $className
+     * @return BaseCatalog
      */
-    private function create(string $propertyName): ?BaseCatalog
+    private function create(string $className): BaseCatalog
     {
-        foreach (['CFDI', 'Nomina'] as $space) {
-            $className = '\\' . __NAMESPACE__ . '\\' . $space . '\\' . ucfirst($propertyName);
-
-            if (! class_exists($className)) {
-                continue;
-            }
-            if (! in_array(BaseCatalog::class, class_implements($className) ?: [], true)) {
-                continue;
-            }
-            /** @var BaseCatalog $object */
-            $object = new $className();
-            $object->withRepository($this->repository);
-
-            return $object;
+        if (! class_exists($className)) {
+            throw new LogicException("$className does not exists");
         }
+        if (! in_array(BaseCatalog::class, class_implements($className) ?: [], true)) {
+            throw new LogicException(sprintf('%s does not implements %s', $className, BaseCatalog::class));
+        }
+        /** @var BaseCatalog $object */
+        $object = new $className();
+        $object->withRepository($this->repository);
 
-        return null;
+        return $object;
     }
 }
