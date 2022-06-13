e10868a957a6d182f1485dfef4b2d319692375c2
Carlos C Soto <eclipxe13@gmail.com>
2022-06-13 13:41:27 -0500

    Fix: remove redundant constructor

--- src/CFDI40/Mes.php
+++ src/CFDI40/Mes.php
@@ -9,12 +9,4 @@ use PhpCfdi\SatCatalogos\Common\EntryIdentifiable;
 
 class Mes extends AbstractEntryIdentifiable implements EntryIdentifiable
 {
-    public function __construct(
-        string $id,
-        string $texto,
-        int $vigenteDesde,
-        int $vigenteHasta
-    ) {
-        parent::__construct($id, $texto, $vigenteDesde, $vigenteHasta);
-    }
 }
